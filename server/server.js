6b5eddbd4a65dfed0c8c47354ba8f492e4d54273
Ryan Possardt <rpossardt@gmail.com>
2018-02-19 18:53:26 -0500

    added api routes, added scripts to run both server and angular in hot start mode

--- server/server.js
+++ server/server.js
@@ -3,18 +3,25 @@ const path         = require('path');
 const logger       = require('morgan');
 const bodyParser   = require('body-parser');
 const http         = require('http');
-const app          = express();
-const PORT         = process.env.PORT || 3000;
+const api          = require('./routes/api');
+const app  = express();
 
+
+//Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use('/api', api);
 
+app.use(express.static(path.join(__dirname, '../dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
+
+//Set port number
+const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 
+//Create the httpserver and start listenin
 const server = http.createServer(app);
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
