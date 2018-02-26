0c1ee193d95c29d20e5b8da9f2f610a280ebea43
Ryan Possardt <ryan_c_possardt@homedepot.com>
2018-02-26 18:41:08 -0500

    gettin there

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -13,9 +13,9 @@ export class TeamComponent {
   show: boolean = false;
   backgroundColors: Array<string> = [
     'blueBackground',
-    'greenBackground'
-    // 'pinkBackground',
-    // 'redBackground'
+    'greenBackground',
+    'pinkBackground',
+    'redBackground'
   ];
   lastSelected: string = '';
   currentColor: string = '';
@@ -25,11 +25,6 @@ export class TeamComponent {
     return this.nameToBackgroundColor[name];
   }
 
-  // setNameColor(name) {
-  //   console.log(this.nameToBackgroundColor);
-  //   this.nameToBackgroundColor[name] = this.currentColor;
-  // }
-
   getInitials(name){
     return name
       .split(' ')
@@ -37,20 +32,47 @@ export class TeamComponent {
       .join('');
   }
 
+  findAndRemovePair(name){
+    for(let i = 0; i < this.team.pairs.length; i++){
+      if(this.team.pairs[i].indexOf(name) !== -1){
+        return this.team.pairs.splice(i, 1)[0];
+      }
+    }
+    return false;
+  }
+
   selectDeveloper(name){
     if(name === this.lastSelected) return;
 
+    //initial state
     if(!this.lastSelected){
       this.lastSelected = name;
-      this.currentColor = this.backgroundColors.pop();
+      //only pop backgroundColor if you need to.
+      this.currentColor = this.currentColor || this.backgroundColors.pop();
       this.nameToBackgroundColor[name] = this.currentColor;
       return;
     }
     else if(this.lastSelected !== name){
+
+      //check if the person is already paired with someone else
+      // if he/she is, remove that pair and add the color back to be
+      // used again.
+      let existingPair = this.findAndRemovePair(name);
+      if(existingPair){
+        console.log(existingPair);
+        let dev1 = existingPair[0];
+        let dev2 = existingPair[0];
+        let color = this.nameToBackgroundColor[dev1];
+        this.backgroundColors.push(color);
+        this.nameToBackgroundColor[dev1] = '';
+        this.nameToBackgroundColor[dev2] = '';
+        this.nameToBackgroundColor[name] = this.currentColor;
+      }
       this.team.pairs.push([name, this.lastSelected]);
       this.nameToBackgroundColor[name] = this.currentColor;
       this.lastSelected = '';
       this.currentColor = this.backgroundColors.pop();
+
       console.log(this.backgroundColors);
     }
     //addPairs
